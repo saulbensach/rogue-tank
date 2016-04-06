251df28f0d136fcd4482fad5c207c37bfde9ca5f
Saï¿ul Bensach <saulcube123@gmail.com>
2016-04-06 23:34:06 +0200

    main menu redirection

--- core/src/com/bensach/saul/screens/MainMenu.java
+++ core/src/com/bensach/saul/screens/MainMenu.java
@@ -1,6 +1,9 @@
 package com.bensach.saul.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.bensach.saul.GameStart;
 
 /**
@@ -9,19 +12,21 @@ import com.bensach.saul.GameStart;
 public class MainMenu implements Screen {
 
     private GameStart gameStart;
+    private Stage stage;
 
     public MainMenu(GameStart gameStart) {
         this.gameStart = gameStart;
+        stage = new Stage();
     }
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
